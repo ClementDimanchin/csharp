36a19c5a678d662670eaefafcd7f3b8924516707
Clément Dimanchin <clementdimanchin@free.fr>
2020-11-03 11:31:13 +0100

    V3 ex1 +protected virtual

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -15,37 +15,54 @@ namespace ex1
     public class Game
     {
         private readonly int valeurATrouver = new Random().Next(0, 100);
+        private readonly int nombreDeCoupsMax = 10;
         int nombreDeCoups = 0;
-        public void Play()
-        {
-            bool trouve = false;
+        bool trouve = false;
+        public void Play(){
             Console.WriteLine("Veuillez saisir un nombre compris entre 0 et 100 (exclu)");
-            while (!trouve)
-            {
+            while (!trouve){
                 string saisie = Console.ReadLine();
                 int valeurSaisie;
-                if (int.TryParse(saisie, out valeurSaisie))
-                {
-                    if (valeurSaisie == valeurATrouver)
-                        trouve = true;
-                    else
-                    {
-                        if (valeurSaisie < valeurATrouver)
-                            Console.WriteLine("Trop petit ...");
-                        else
-                            Console.WriteLine("Trop grand ...");
+                if (int.TryParse(saisie, out valeurSaisie)){
+                   trouve = Verif(valeurSaisie);
+                }else{
+                    if(valeurSaisie == -1){
+                        End(false);
+                        break;
+                    }else{
+                     Console.WriteLine("La valeur saisie est incorrecte, veuillez recommencer ...");
                     }
-                    nombreDeCoups++;
                 }
-                else
-                    Console.WriteLine("La valeur saisie est incorrecte, veuillez recommencer ...");
             }
-            End();
+            End(trouve);
         }
 
-        public void End(){
-            Console.WriteLine("Vous avez trouvé en " + nombreDeCoups + " coup(s)");
-            Console.ReadLine();
+        public virtual bool Verif(int valeurSaisie){
+            if(nombreDeCoups < nombreDeCoupsMax){
+                if(valeurSaisie == valeurATrouver){
+                    return true;
+                }else{
+                    if (valeurSaisie < valeurATrouver){
+                        Console.WriteLine("Trop petit ...");
+                    }else{
+                        Console.WriteLine("Trop grand ...");
+                    };
+                    nombreDeCoups++;
+                    return false;
+                };
+            }else{
+                return true;
+            }
+        }
+
+        public void End(bool success){
+            if(success){
+                Console.WriteLine("Vous avez trouvé en " + nombreDeCoups + " coup(s)");
+                Console.ReadLine();
+            }else{
+                Console.WriteLine("Vous n'avez pas trouvé en moins de " + nombreDeCoupsMax + " coup(s)");
+                Console.ReadLine();
+            }
             Console.WriteLine("Fin du jeu");
         }
 
